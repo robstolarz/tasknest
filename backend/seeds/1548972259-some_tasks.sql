7ea4b643c3142e6f912c850552f464b6fe605f41
Robert Stolarz <robert.stolarz@yandex.com>
2019-02-10 16:38:27 -0500

    Allow user to add new tasks after seeding

--- backend/seeds/1548972259-some_tasks.sql
+++ backend/seeds/1548972259-some_tasks.sql
@@ -3,3 +3,4 @@ INSERT INTO tasks (id, title, parent) VALUES
   (2, 'Display tasks', 1),
   (3, 'Display a nested list of tasks', 2),
   (4, 'Retrieve the nested list of tasks from the backend', 2);
+ALTER SEQUENCE tasks_id_seq RESTART WITH 5;
